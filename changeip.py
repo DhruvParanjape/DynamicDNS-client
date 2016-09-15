e76eb10d1141ec2dad25cd38023bce8b85c2370a
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-15 08:23:59 +0530

    refactor to use functions and use Logger

--- changeip.py
+++ changeip.py
@@ -1,12 +1,7 @@
 from requests import get
 
-username = "dhruv.paranjape@quickheal.com"
-password = "admin@123"
-domain = "dhruv1.authorizeddns.us"
-ip = "4.3.2.1"
+def ChangeIP(username,password,domain,ip):
+  fetch_url = "https://nic.changeip.com/nic/update?hostname=%s&myip=%s" %(domain, ip)
 
-fetch_url = "https://nic.changeip.com/nic/update?hostname=%s&myip=%s" %(domain, ip)
-
-response = get(fetch_url, auth=(username, password))
-
-print(response.text)
+  response = get(fetch_url, auth=(username, password))
+  Log(response.text)
