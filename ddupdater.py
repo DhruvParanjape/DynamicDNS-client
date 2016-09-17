034e95f383b7bb3e149d091fb3b19aff7949aa45
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-17 18:22:07 +0530

    Add daemon support

--- ddupdater.py
+++ ddupdater.py
@@ -1,3 +1,17 @@
 from freedns import freeDNS
 from duckdns import DuckDNS
 from Changeip import ChangeIP
+from os import fork
+from time import sleep
+
+#daemonize
+process_id = fork()
+
+if process_id != 0:
+
+	while  True:
+		#Read the config
+		#get ip from given interface
+		#update url
+		#sleep for expected time.
+		sleep(5)
