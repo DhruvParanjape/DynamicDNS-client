985e7cdbb608065d9f7cbdfb646a1b756ad3983a
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-18 08:58:54 +0530

    forgot the removal

--- Configurators/Interface.py
+++
@@ -1,4 +0,0 @@
-from netifaces import ifaddresses
-
-def GetIPfromInterface(interface_name):
-	return ifaddresses(interface_name)[2][0]['addr']
--- providers/changeip.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-
-def ChangeIP(username,password,domain,ip):
-  fetch_url = "https://nic.changeip.com/nic/update?hostname=%s&myip=%s" %(domain, ip)
-
-  response = get(fetch_url, auth=(username, password))
-  Log(response.text)
