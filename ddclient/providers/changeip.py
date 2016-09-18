524eeb800c5384cb45d1b7da07f24695ccc14d4e
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-18 17:43:52 +0530

    Add missing logger import

--- ddclient/providers/changeip.py
+++ ddclient/providers/changeip.py
@@ -1,4 +1,5 @@
 from requests import get
+from ddclient.Configurators.Logger import Log
 
 def ChangeIP(username,password,domain,ip):
   fetch_url = "https://nic.changeip.com/nic/update?hostname=%s&myip=%s" %(domain, ip)
