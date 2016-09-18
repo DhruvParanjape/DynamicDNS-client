7975917df678e409b01bfb2de8259c8a892ca236
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-18 17:54:57 +0530

    Add caching support for freedns provider
    Freedns uses a single update url.
    Thus if the domain does not change the entire process of finding the update url is unessary.
    cache the update url and if domain does not change reuse the update url.

--- ddclient/providers/freedns.py
+++ ddclient/providers/freedns.py
@@ -4,20 +4,32 @@ from ddclient.Configurators.Logger import Log
 
 def FreeDNS(username, password, domain):
 	fetch_url = "https://freedns.afraid.org/api/?action=getdyndns&v=2&sha="
+	cache_filename = "ddclient.cache"
+	cache = ""
 	update_url = ""
-	hash_string = username+"|"+password
+	with open(cache_filename,"w") as f:
+		cache = f.read()
+	if cache.split("|")[-1] == domain:
+		update_url = cache.split("|")[0]
+		Log("sending "+update_url)
+		update_response = get(update_url)
+		#Updated status written in log file.
+		Log(update_response.text)
+	else:
+		hash_string = username+"|"+password
+		token = sha1(str.encode(hash_string))
+		response = get(fetch_url+token.hexdigest())
+		data = response.text.split("\n")
 
-	token = sha1(str.encode(hash_string))
-
-	response = get(fetch_url+token.hexdigest())
-	data = response.text.split("\n")
-
-	for line in data:
-		param = line.split("|")
-		if domain == param[0]:
-			update_url = param[-1]
-	Log("sending "+update_url)
+		for line in data:
+			param = line.split("|")
+			if domain == param[0]:
+				update_url = param[-1]
+		Log("sending "+update_url)
 	
-	update_response = get(update_url)
-	#Updated status written in log file.
-	Log(update_response.text)
+		update_response = get(update_url)
+		#Updated status written in log file.
+		Log(update_response.text)
+		#write update url with the domain to the cache.
+		with open(cache_filename,"w") as f:
+			f.write(update_url+"|"+domain)
