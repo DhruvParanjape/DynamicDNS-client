97da7895f0db7dfa3ba6a96711ac938d3611d670
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-18 17:45:31 +0530

    Add ddupdater.py code

--- ddupdater.py
+++ ddupdater.py
@@ -1,16 +1,33 @@
-from freedns import freeDNS
-from duckdns import DuckDNS
-from Changeip import ChangeIP
+from ddclient.providers.freedns import freeDNS
+from ddclient.providers.duckdns import DuckDNS
+from ddclient.providers.Changeip import ChangeIP
+from ddclient.Configurators.Logger import Log
+from ddclient.Configurators.ConfigurationReader import FetchParameters
+from ddclient.Configurators.Interface import GetIPfromInterface
 from daemon import DaemonContext
 from time import sleep
 
-#daemonize
 
+#config file path
+config_file_path = "/etc/ddupdater.conf"
+
+#daemonize
 with DaemonContext():
 
 	while  True:
 		#Read the config
-		#get ip from given interface
-		#update url
+		provider, username, password, domain, iface_name, update_interval = FetchParameters(config_file_path)
+		if (provider == freeDNS):
+			freeDNS(username, password, domain)
+		elif (provider == DuckDNS):
+			ip = GetIPfromInterface(iface_name)
+			DuckDNS(password,domain,ip)
+		elif (provider == ChangeIP):
+			ip = GetIPfromInterface(iface_name):
+			ChangeIP(username,password,domain,ip)
+		else :
+			Log("Unknown provider configured shutting down...")
+			Log("Provider configured : "+provider)
+		
 		#sleep for expected time.
-		sleep(5)
+		sleep(update_interval)
