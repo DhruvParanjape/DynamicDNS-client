a8c608acb757b8e78d81ae7e884c8ca021caa3b7
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-09-20 07:50:13 +0530

    Add support to specify ip address when updating
    Switch to a newer method of updating dynamic dns for freedns provider
    and support older method as an alternative method.(FreeDNS2)
    Add support to update with ip for both.

--- ddclient/providers/freedns.py
+++ ddclient/providers/freedns.py
@@ -3,7 +3,13 @@ from requests import get
 from os.path import isfile
 from ddclient.Configurators.Logger import Log
 
-def FreeDNS(username, password, domain):
+def FreeDNS_2(username, password, domain):
+	url = "https://%s:%s@freedns.afraid.org/nic/update?hostname=%s&myip=%s" %(username, password, domain, ip)
+		update_response = get(url)
+		#Updated status written in log file.
+		Log(update_response.text)
+
+def FreeDNS2(username, password, domain, ip):
 	fetch_url = "https://freedns.afraid.org/api/?action=getdyndns&v=2&sha="
 	cache_filename = "ddclient.cache"
 	cache = ""
@@ -12,7 +18,7 @@ def FreeDNS(username, password, domain):
 		with open(cache_filename,"w") as f:
 			cache = f.read()
 	if cache.split("|")[-1] == domain:
-		update_url = cache.split("|")[0]
+		update_url = cache.split("|")[0] + "&address=%s" %(ip)
 		Log("sending "+update_url)
 		update_response = get(update_url)
 		#Updated status written in log file.
